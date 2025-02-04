360b74528c14ec5b6517f6caaee75e5b9708fee6
Студент <@PC65-10>
2025-02-04 13:40:46 +0300

    Добавил приложение для сложения 2 чисел

---
+++ Compiler/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрашиваем у пользователя первое число
+        Console.Write("Введите первое число: ");
+        string input1 = Console.ReadLine();
+        int number1 = Convert.ToInt32(input1);
+
+        // Запрашиваем у пользователя второе число
+        Console.Write("Введите второе число: ");
+        string input2 = Console.ReadLine();
+        int number2 = Convert.ToInt32(input2);
+
+        // Складываем два числа
+        int sum = number1 + number2;
+
+        // Выводим результат
+        Console.WriteLine($"Сумма {number1} и {number2} равна {sum}");
+    }
+}
+
